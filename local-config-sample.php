0d9f22b1d8883fd63bb680cb3222f8815f4f402e
morgpion <morg.leen@ouvaton.org>
2021-02-03 13:19:03 +0100

    ajout local-config-sample.php

---
+++ local-config-sample.php
@@ -0,0 +1,17 @@
+
+<?php 
+
+/**  SAMPLE
+ * Remplacer les valeurs des constantes par vos informations de bdd et connexion
+ */
+
+// base URL for all relative URLs
+define("BASE_URL", "l_url_du_site");
+
+// Server MYSQL
+define("SQL_SERVER", "serveur_de_votre_bdd");
+define("SQL_DATABASE_NAME", "nom_de_votre_bdd");
+define("SQL_USER", "nom_d_utilisateur");
+define("SQL_PASS", "votre_mdp");
+
+
